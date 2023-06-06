2997ab6f47ea60b0af50d265379f243a3bfc2614
Faruqoloyede <oloyedefaru2@gmail.com>
2023-06-06 08:05:17 -0700

    initial commit on mobile view

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,20 +11,5 @@ const scrollheader = () => {
 window.addEventListener("scroll", scrollheader);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    // reset: true
-});
 
-sr.reveal(`.home_header, .section_title`,{delay: 600})
-sr.reveal(`.home_footer`,{delay: 700})
-sr.reveal(`.home_img`,{delay: 900, origin: 'top'})
-
-sr.reveal(`.sponsor_img, .product_card, .footer_logo, .footer_content, .footer_copy`,{origin: 'top', interval: 100})
-sr.reveal(`.specs_data,`,{origin: 'left', interval: 100})
-sr.reveal(`.specs_img, .discount_img`,{origin: 'right'})
-sr.reveal(`.case_img`,{origin: 'top'})
-sr.reveal(`.case_data`)
 

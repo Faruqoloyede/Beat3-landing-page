38b39cea2463874723f6d137bdd5a261750a62b4
Faruqoloyede <oloyedefaru2@gmail.com>
2023-06-06 10:59:02 -0700

    final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
+const nav = document.getElementById("nav_menu");
+const menu = document.getElementById("menu_btn");
+
+menu.addEventListener("click", ()=>{
+    menu.classList.toggle("open");
+    nav.classList.toggle("show");
+});
+
+
+
+// const sr = ScrollReveal({
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 400,
+//     // reset: true
+// })
+
+
+
+
 //when the scroll is greater than 50 viewport height add the scroll-header class to the tag
 const scrollheader = () => {
     const header = document.getElementById("header");
@@ -10,6 +30,6 @@ const scrollheader = () => {
 
 window.addEventListener("scroll", scrollheader);
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+
 
 
